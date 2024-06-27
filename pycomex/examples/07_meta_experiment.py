f9bc7559439e480f1509ced99e23c05df3cdabf7
awa59kst120df <awa59kst120df@gmail.com>
2024-06-27 15:13:00 +0200

    added more examples

---
+++ pycomex/examples/07_meta_experiment.py
@@ -0,0 +1,61 @@
+"""
+Based on the functionality introduced in the previous example, we can take the whole idea one 
+step further with so-called "meta experiments". These are experiment modules themselves but their 
+implementation essentially relies on the execution of various other experiment modules as part 
+of it's own runtime.
+
+**DIFFERENCE TO EXPERIMENT INHERITANCE**
+
+Meta experiments are a slightly different use case than experiment inheritance. In experiment 
+inheritance, the focus is rather on somehow substantially modifying the behavior of the original 
+base experiment and extending it in some way. Most crucially, the experiment itself stays the 
+top most abstraction layer and it is executed at most once during the same runtime.
+
+With meta experiments, the idea is to facilitate the execution of multiple experiments as part 
+of a larger experiment that kind of compiles the results of these sub experiments in some sense.
+
+The most obvious use case of meta experiments is to easily perform some kind of ablation studies or 
+parameter sweeps. It is possible to modify the parameters of these experiements before executing 
+them. In that way one could define a loop in which one (or many) parameters of another experiment 
+are iteratively systematically changed.
+"""
+import os
+import pathlib
+import typing as t
+
+from pycomex.functional.experiment import Experiment, get_experiment
+from pycomex.utils import file_namespace, folder_path, random_string
+
+PATH = pathlib.Path(__file__).parent.absolute()
+
+# :param REPETITIONS:
+#       pass
+REPETITIONS: int = 3
+
+# :param NUM_WORDS_SWEEP:
+#       pass
+NUM_WORDS_SWEEP: t.List[int] = [10, 100, 1000]
+
+
+@Experiment(
+    base_path=folder_path(__file__),
+    namespace=file_namespace(__file__),
+    glob=globals()    
+)
+def experiment(e: Experiment):
+    
+    e.log('starting meta experiment...')
+    
+    for num_words in e.NUM_WORDS_SWEEP:
+        
+        e.log(f'running experiment with {num_words} number of words')
+        exp: Experiment = get_experiment(os.path.join(PATH, '03_analysing.py'))
+        exp.NUM_WORDS = num_words
+        exp.logger = e.logger
+        exp.name = 'meta_' + random_string()
+        exp.run()
+
+        e[f'metrics/length/{num_words}'] = sum(exp['metrics/length'].values())
+
+
+experiment.run_if_main()
---
+++ pycomex/examples/08_weights_and_biases.py
@@ -0,0 +1,59 @@
+"""
+This example showcases the weights and biases integration for pycomex. The weights and biases plugin
+is enabled by default and an experiment can be directly logged to the wandb service by meeting the 
+following two criteria: 
+
+- The WANDB_API_KEY environment variable is set in the environment from which the experiment was 
+  started.
+- The experiment defines the WANDB_PROJECT parameter at the beginning of the file as a non-empty 
+  string identifier for an exsiting wandb project.
+"""
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+
+from pycomex.functional.experiment import Experiment
+from pycomex.utils import folder_path, file_namespace
+
+# :param N_ELEMENTS:
+#      The number of elements to be randomly generated for the scatter plot.
+N_ELEMENTS: int = 100
+# :optparam WANDB_PROJECT:
+#      The name of the wandb project to which the experiment should be logged.
+#      This is required to activate the weights and biases integration!
+WANDB_PROJECT: str = 'test'
+
+__DEBUG__ = True
+
+experiment = Experiment(
+    base_path=folder_path(__file__),
+    namespace=file_namespace(__file__),
+    glob=globals(),
+)
+
+@experiment
+def experiment(e: Experiment):
+    
+    e.log('starting experiment...')
+    # The experiment "track" method can be used to track the numeric (float) values of certain 
+    # metrics for example. These will be logged to the wandb service directly and can be 
+    # observed in the online dashboard in real-time.
+    e.track('time', time.time())    
+
+    e.log('creating plot...')
+    e.track('time', time.time())
+    fig, ax = plt.subplots(
+        ncols=1,
+        nrows=1,
+    )
+    data = np.random.rand(N_ELEMENTS, 2)
+    ax.scatter(data[:, 0], data[:, 1])
+    
+    # The experiment "commit_fig" method can be used to commit a figure to the wandb service as
+    # an image artifact. This can be observed in the online dashboard as well. The figure will be
+    # saved to the experiment folder as well.
+    e.commit_fig('figure.pdf', fig)
+    
+    e.track('time', time.time())
+
+experiment.run_if_main()
